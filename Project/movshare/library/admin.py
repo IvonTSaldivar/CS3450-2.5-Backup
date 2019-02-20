7e3076b7af036a7014431214e459e4c139b757f9
Mitch Shelton <>
2019-02-19 17:07:13 -0700

    added admin stuff

--- Project/movshare/library/admin.py
+++ Project/movshare/library/admin.py
@@ -16,3 +16,14 @@
 #    list_display = ["username", "name", "is_superuser"]
 #    search_fields = ["name"]
 
+from django.contrib import admin
+
+from movshare.library.models import Shelf, Media
+
+@admin.register(Shelf)
+class ShelfAdmin(admin.ModelAdmin):
+	list_display = ('name','owner',)
+
+@admin.register(Media)
+class MediaAdmin(admin.ModelAdmin):
+	list_display = ('name','media_type','owner','shelf',)
