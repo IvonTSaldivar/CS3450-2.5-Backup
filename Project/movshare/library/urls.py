453dc6c5a154c819b2f7b64f0b1dd82929a55414
Ivon T Saldivar <saldivar.ivon@ichsstudent.org>
2019-03-13 23:00:01 -0600

    Rudimentary stuff working. Currently addMedia doesn't show shelf names for some reason

--- Project/movshare/library/urls.py
+++ Project/movshare/library/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 
 from movshare.library.views import (
-    ShelfView,
+    ShelfView, AddMediaView, AddShelfView, PostMedia, PostShelf,
 )
 
 app_name = "library"
 urlpatterns = [
     path("shelf", view=ShelfView, name="shelf"),
+	path("addMedia", view=AddMediaView, name="addMedia"),
+	path("addShelf", view=AddShelfView, name="addShelf"),
+	path("newMedia", view=PostMedia, name="newMedia"),
+	path("newShelf", view=PostShelf, name="newShelf"),
+
 ]
--- Project/movshare/library/views.py
+++ Project/movshare/library/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from movshare.library.models import Shelf
 from movshare.library.models import Media
+from django.contrib.auth import get_user_model
 
 # Create your views here.
 
@@ -15,3 +16,32 @@ def ShelfView(request):
                  )
 
 #user_list_view = UserListView.as_view()
+
+def AddMediaView(request):
+	shelves = Shelf.objects.all();
+	return render(request, 'pages/addViews/addmedia.html',
+					{
+						'shelves': shelves
+					 }
+				 )
+	
+def AddShelfView(request):
+	return render(request, 'pages/addViews/addshelf.html')
+	
+def PostMedia(request):
+	if request.method == 'POST':
+		m = Media(name=request.POST.get("name"),
+						owner=request.user,
+						media_type=request.POST.get("type"),
+						description=request.POST.get("description"),
+						shelf=Shelf.objects.get(name=request.POST.get("shelf")))
+		m.save()
+	return redirect('library:shelf')
+	
+def PostShelf(request):
+	if request.method == 'POST':
+		s = Shelf(name=request.POST.get("name"),
+						owner=request.user)
+		s.save()
+	return redirect('library:shelf')
+	
