aba7b8c34a7f89b4b59895d1d4d8aa7312ddce60
Ivon Saldivar <saldivar.ivon@ichsstudent.org>
2019-03-29 14:56:19 -0600

    Not entirely sure what I'm doing

--- Project/movshare/static/js/project.js
+++ Project/movshare/static/js/project.js
@@ -1,1 +1,25 @@
 /* Project specific Javascript goes here. */
+
+function toggleHideContent(var id){
+    var content = document.getElementById(id);
+    if(content)
+        if(content.style.display == "hidden"){
+            content.style.display = "block";
+        } else if (content.style.display == "block"){
+            content.style.display = "hidden";
+        }
+    }
+}
+
+function toggleButton(var id){
+    toggleHideContent(id);
+    toggleOtherButton(id);
+}
+function toggleOtherButton(var id){
+    var otherID;
+    if (id == "addMediaButton"){
+        otherID = "addShelfButton";
+    } else otherID = "addMediaButton";
+    var button = document.getElementById(otherID);
+    button.style.display = "hidden";
+}
