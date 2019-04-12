38972fc6764f785c2605e514bfc9ad709448d78a
garryjable <garrettjamesjennings@gmail.com>
2019-04-12 00:45:26 -0600

    working on the new model changes quick

--- Project/movshare/library/tests/test_models.py
+++ Project/movshare/library/tests/test_models.py
@@ -11,8 +11,9 @@ class LibraryModelsTest(TestCase):
 
     def setUp(self):
         self.user = User.objects.create(username='root', email='root@root.root', password='iamroot96!')
+        self.borrower = User.objects.create(username='borrow', email='borrow@borrow.borrow', password='iamborrow96!')
         self.shelf = Shelf.objects.create(name="myShelf", owner=self.user)
-        self.media = Media.objects.create(name="myMedia", media_type="movie", description="a movie", owner=self.user, shelf=self.shelf)
+        self.media = Media.objects.create(name="myMedia", media_type="movie", description="a movie", owner=self.user, borrower=self.borrower, isBorrowed=true, shelf=self.shelf)
 
     def test_shelf_model(self):
         self.assertEqual(self.shelf.name, "myShelf")
@@ -23,4 +24,6 @@ class LibraryModelsTest(TestCase):
         self.assertEqual(self.media.media_type, "movie")
         self.assertEqual(self.media.description, "a movie")
         self.assertEqual(self.media.owner, self.user)
+        self.assertEqual(self.media.borrower, self.borrower)
+        self.assertEqual(self.media.isBorrowed, self.true)
         self.assertEqual(self.media.shelf, self.shelf)
