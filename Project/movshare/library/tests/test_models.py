feacb0c54b0e8aaf0df013bffedbaa785be98e80
garryjable <garrettjamesjennings@gmail.com>
2019-04-12 00:48:37 -0600

    typo

--- Project/movshare/library/tests/test_models.py
+++ Project/movshare/library/tests/test_models.py
@@ -13,7 +13,7 @@ class LibraryModelsTest(TestCase):
         self.user = User.objects.create(username='root', email='root@root.root', password='iamroot96!')
         self.borrower = User.objects.create(username='borrow', email='borrow@borrow.borrow', password='iamborrow96!')
         self.shelf = Shelf.objects.create(name="myShelf", owner=self.user)
-        self.media = Media.objects.create(name="myMedia", media_type="movie", description="a movie", owner=self.user, borrower=self.borrower, isBorrowed=true, shelf=self.shelf)
+        self.media = Media.objects.create(name="myMedia", media_type="movie", description="a movie", owner=self.user, borrower=self.borrower, isBorrowed=True, shelf=self.shelf)
 
     def test_shelf_model(self):
         self.assertEqual(self.shelf.name, "myShelf")
@@ -25,5 +25,5 @@ class LibraryModelsTest(TestCase):
         self.assertEqual(self.media.description, "a movie")
         self.assertEqual(self.media.owner, self.user)
         self.assertEqual(self.media.borrower, self.borrower)
-        self.assertEqual(self.media.isBorrowed, self.true)
+        self.assertEqual(self.media.isBorrowed, self.True)
         self.assertEqual(self.media.shelf, self.shelf)
