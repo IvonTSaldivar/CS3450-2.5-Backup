a9a68f3bf787aecb6c7ad6442c3b2a4c49986d6c
Mitch Shelton <>
2019-04-16 00:03:47 -0600

    Added some tests for IMDB integration, in a hidden page at the moment.

--- Project/movshare/library/tables.py
+++ Project/movshare/library/tables.py
@@ -16,3 +16,10 @@ class ExpandedShelfTable(tables.Table):
         fields = ['name', 'media_type', 'description', 'delete']
         template_name = 'django_tables2/bootstrap.html'
     delete = TemplateColumn(template_name='pages/tables/deletemediabtn.html')
+
+class IMDBSearchTable(tables.Table):
+    class Meta:
+        model = Media
+        attrs = {'class': 'table table-sm'}
+        fields = ['title', 'type', 'year']
+        template_name = 'django_tables2/bootstrap.html'
